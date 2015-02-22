9741cdbe111ebd94acbf62b7931ddd9b42364e14
sakabar <takafumi.sakakibara@gmail.com>
2015-02-22 15:05:15 +0900

    Modify: uniq problem

--- script/tutcode_practice.sh
+++ script/tutcode_practice.sh
@@ -9,13 +9,13 @@ negative_log="" #不正解の漢字の一覧
 BASEDIR=$(cd $(dirname $0);pwd)
 log_dir=$BASEDIR"/../log"
 
-len=`grep '"[a-z]" "[a-z]" "[a-z]"' $problem | grep -v ";;" | wc -l | grep -o "[0-9]\+"`
+len=`grep '"[a-z]" "[a-z]" "[a-z]"' $problem | grep -v ";;" | sort | uniq | wc -l | grep -o "[0-9]\+"`
 echo "$len個の漢字の中から出題されます。"
 
 for i in {1..$iter}
 do
   rnd=$[$RANDOM % $len + 1]
-  line=`grep '"[a-z]" "[a-z]" "[a-z]"' $problem | head -n $rnd | tail -n1`
+  line=`grep '"[a-z]" "[a-z]" "[a-z]"' $problem | grep -v ";;" | sort | uniq | head -n $rnd | tail -n1`
   stroke=`echo $line | grep -o '"[a-z]"' | tr -d '\n' | tr -d '"'`
   kanji=`echo $line | grep -o '"."' | grep -v "[a-z]" | tr -d '"' | nkf -w`
   kanji_log=$kanji_log$kanji
